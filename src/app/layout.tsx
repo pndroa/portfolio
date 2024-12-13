94be10df1e9a1e8ba973b9663c1d9b44045cbadd
pndroa <157326278+pndroa@users.noreply.github.com>
2024-12-13 17:16:39 +0100

    created next js project

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Erguen Bickici",
+  description: "Portfolio of Erguen Bickici made by Erguen Bickici",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
