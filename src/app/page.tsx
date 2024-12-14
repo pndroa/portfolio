bb6216eac82e12bc112eb771984acf54b9333375
pndroa <157326278+pndroa@users.noreply.github.com>
2024-12-14 19:38:15 +0100

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,30 +67,7 @@ export default function Home() {
       </Header>
       <Box>
         <Section ref={homeRef}>
-          <Card isFooterBlurred className="border-none" radius="lg">
-            <Image
-              alt="Woman listing to music"
-              className="object-cover"
-              height={200}
-              src="https://nextui.org/images/hero-card.jpeg"
-              width={200}
-            />
-            <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-              <p className="text-tiny text-white/80">Available soon.</p>
-              <Button
-                className="text-tiny text-white bg-black/20"
-                color="default"
-                radius="lg"
-                size="sm"
-                variant="flat"
-                onPress={() => {
-                  alert("Thanks for visiting my Website :3")
-                }}
-              >
-                Notify me
-              </Button>
-            </CardFooter>
-          </Card>
+          Home  
         </Section>
         <Section ref={aboutRef}>About Me</Section>
         <Section ref={cvRef}>CV</Section>
